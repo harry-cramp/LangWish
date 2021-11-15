a9ebb97771a57f49fb599ccca15602085830c193
harry-cramp <harrycramp@hotmail.com>
2021-11-15 23:06:58 +0000

    Create flashcard deck menu

---
+++ app/src/main/java/com/anaglyph/langwish/DeckMenu.kt
@@ -0,0 +1,14 @@
+package com.anaglyph.langwish
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class DeckMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load_decks)
+
+
+    }
+}
